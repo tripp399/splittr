f0d653867f666f6917bcdbe3c63167ef9fd122a8
Pulkit Tripathi <p.tripathi@ufl.edu>
2020-02-06 23:59:55 -0500

    tesdb configuration and test data for unit testing

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -4,10 +4,11 @@ import pytest
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from api.run import app, db as tdb
+from test.test_util import initilize_test_db
 
-test_db_uri = 'postgresql://myuser:mypass@localhost:5432/mydb'
+test_db_uri = 'postgresql://myuser:mypass@localhost:5432/testdb'
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def _test_app():
     test_app = app
     test_app.testing = True
@@ -18,11 +19,11 @@ def _test_app():
     test_app.app_context().push()
     return test_app
 
-@pytest.fixture
+@pytest.fixture()
 def client(_test_app):
     return _test_app.test_client()
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def root_url():
     return "http://localhost:5000/"
 
@@ -33,19 +34,13 @@ def db():
 @pytest.fixture(scope="session")
 def _test_db(_test_app):
     test_app = _test_app
-    # test_app.config['SQLALCHEMY_DATABASE_URI'] = test_db_uri
-    # test_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # test_app.config['PROPAGATE_EXCEPTIONS'] = True
-    # test_app.config['TESTING'] = True
-    # test_app.app_context().push()
-    test_db = SQLAlchemy(test_app)
-    test_db.create_all()
-    yield test_db
-
-    test_db.session.commit()
-    test_db.drop_all()
-
-
+    tdb.init_app(test_app)
+    try:
+        initilize_test_db(tdb)
+        yield tdb
+    finally:
+        tdb.session.commit()
+        tdb.drop_all()
 
 @pytest.fixture
 def data():
--- backend/test/test_auth.py
+++ backend/test/test_auth.py
@@ -2,15 +2,15 @@ class TestAuthentication:
 
     base_url = 'http://localhost:5000'
 
-    def test_incorrect_login(self, client, data):
-        user = data['users']['incorrect_user']
+    def test_incorrect_login(self, client, data, _test_db):
+        user = data['incorrect_user']
         post_data = {'username': user['username'], 'pwHash': user['pw_hash']}
         response = client.post(self.base_url + '/login', json=post_data)
         print(response)
         assert response.status_code == 401
 
-    def test_correct_login(self, client, data):
-        user = data['users']['user1']
+    def test_correct_login(self, client, data, _test_db):
+        user = data['users'][0]
         post_data = {'username': user['username'], 'pwHash': user['pw_hash']}
         response = client.post(self.base_url + '/login', json=post_data)
         assert response.status_code == 200
