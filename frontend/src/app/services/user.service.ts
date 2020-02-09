4c441ab373fc933d41068cfc8cc538f81952bf3d
Pulkit Tripathi <p.tripathi@ufl.edu>
2020-02-09 01:41:38 -0500

    filtered current user from search results

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { User } from '../models/user';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class UserService {
 
   apiUrl: string = 'http://localhost:5000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   register(username: string, name: string, pwHash: string) {
     return this.http.post(this.apiUrl + '/signup', { username, name, pwHash });
@@ -22,11 +23,12 @@ export class UserService {
       return of([]);
     }
     const options = { params: new HttpParams().set('name', filter.name)};
-    return this.http.get<User[]>(this.apiUrl + '/users', options)
+    return this.http.get<any[]>(this.apiUrl + '/users', options)
       .pipe(
         map(response => {
-          console.log('response:');
-          console.log(response);
+          response = response.filter(user => {
+            return user.user_id !== this.authService.currentUserValue.id;
+           });
           return response;
         })
       );
