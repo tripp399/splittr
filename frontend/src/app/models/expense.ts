8c137400ba08d67064bc0726124932b83b172619
Pulkit Tripathi <p.tripathi@ufl.edu>
2020-02-10 13:01:22 -0500

    created group component and expense model

---
+++ frontend/src/app/models/expense.ts
@@ -0,0 +1,47 @@
+import { propertyMap } from '../_helpers/model-mapper';
+
+enum SplitType {
+    EQUAL = 'equal',
+    EXACT = 'exact',
+    PERCENTAGE = 'percentage'
+}
+
+export class Expense {
+    @propertyMap('expense_id')
+    id: number;
+    title: string;
+    total: number;
+    payer: number;
+    @propertyMap('group_id')
+    groupId: number;
+    split: SplitType;
+    timestamp: string;
+    shareMap: Map<number, number>;
+
+    constructor(id?: number, groupId?: number) {
+        this.id = id;
+        this.groupId = groupId;
+        this.shareMap = new Map<number, number>();
+    }
+
+    // TODO: convert to generic util function
+    toCustomObj() {
+        return {
+            id: this.id,
+            title: this.title,
+            total: this.total,
+            payer: this.payer,
+            groupId: this.groupId,
+            split: this.split,
+            shareMap: this.mapToObject(this.shareMap)
+        };
+    }
+
+    private mapToObject(inputMap) {
+        const obj = {};
+        inputMap.forEach((value, key) => {
+            obj[key] = value;
+        });
+        return obj;
+    }
+}
